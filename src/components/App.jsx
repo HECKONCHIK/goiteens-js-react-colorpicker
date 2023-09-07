c59119c982ea0f7fc8a1296e6545f8e52a169ff6
Oleh Tsap <olehtsap07@gmail.com>
2023-09-07 19:40:10 +0300

    todolist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,74 @@
 import { Component } from "react";
+// import  GlobalStyle from "./GlobalStyle";
 import "../todo.json"
-import { StickerList } from "./Sticker/StickerList";
-import stickers from "../sticker.json"
+import { TodoEditor } from "./TodoEditor/TodoEditor";
+import TodoList from "./TodoList/TodoList";
+import { nanoid } from 'nanoid';
+import initialTodos from '../todo.json';
+// import { StickerList } from "./Sticker/StickerList";
+// import stickers from "../sticker.json"
  
 
 export class App extends Component {
-  state = {
-    stickerLabel: null
+  
+  
+  state={
+    todos: initialTodos,
+  }
+ 
+  addTodo = (text)=>{
+    const newTodo = {
+      id: nanoid(),
+      text,
+      completed: false
+    }
+    
+  this.setState((prevState)=>{
+    return {
+      todos: [newTodo, ...prevState.todos]
+    }
+  })
   }
 
-  labelHandler = label => {
-    this.setState({stickerLabel: label})
+  render(){
+    return (
+      <>
+      <TodoEditor addTodo={this.addTodo}/>
+      <TodoList todos={this.state.todos}/>
+      {/* <GlobalStyle/> */}
+      </>
+    );
   }
-
-  render() {
-    const {stickerLabel} = this.state 
-      return (
-    <>
-      { stickerLabel && <h1>{stickerLabel}</h1>}
-      <StickerList stickers={stickers} labelHandler={this.labelHandler} />
-       {/* <ColorPicker options={colorOptions}/> */}
-  </>
-  );
-  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // state = {
+  //   stickerLabel: null
+  // }
+  
+  // labelHandler = label => {
+  //   this.setState({stickerLabel: label})
+  // }
+  
+  // render() {
+  //   const {stickerLabel} = this.state
+  //     return (
+  //   <>
+  //     { stickerLabel && <h1>{stickerLabel}</h1>}
+  //     <StickerList stickers={stickers} labelHandler={this.labelHandler} />
+  //      {/* <ColorPicker options={colorOptions}/> */}
+  // </>
+  // );
+  // }
 };
